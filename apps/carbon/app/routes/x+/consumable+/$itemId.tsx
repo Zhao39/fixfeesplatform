crbnos/carbon

feat: add custom fields to the properties sidebars

apps/carbon/app/routes/x+/consumable+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getItemFiles,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -30,11 +31,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [consumableSummary, buyMethods, pickMethods] = await Promise.all([
+  const [consumableSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getConsumable(client, itemId, companyId),
-
     getBuyMethods(client, itemId, companyId),
     getPickMethods(client, itemId, companyId),
+    getTagsList(client, companyId, "consumable"),
   ]);
 
   if (consumableSummary.error) {
@@ -52,6 +53,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(client, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/fixture+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getItemFiles,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -29,10 +30,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [fixtureSummary, buyMethods, pickMethods] = await Promise.all([
+  const [fixtureSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getFixture(client, itemId, companyId),
     getBuyMethods(client, itemId, companyId),
     getPickMethods(client, itemId, companyId),
+    getTagsList(client, companyId, "fixture"),
   ]);
 
   if (fixtureSummary.error) {
@@ -50,6 +52,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(client, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/job+/$jobId.tsx
@@ -28,6 +28,7 @@ import {
   JobHeader,
   JobProperties,
 } from "~/modules/production";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -45,7 +46,10 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { jobId } = params;
   if (!jobId) throw new Error("Could not find jobId");
 
-  const job = await getJob(client, jobId);
+  const [job, tags] = await Promise.all([
+    getJob(client, jobId),
+    getTagsList(client, companyId, "job"),
+  ]);
 
   if (job.error) {
     throw redirect(
@@ -56,6 +60,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
   return defer({
     job: job.data,
+    tags: tags.data ?? [],
     files: getJobDocuments(client, companyId, job.data),
     method: getJobMethodTree(client, jobId), // returns a promise
   });

apps/carbon/app/routes/x+/material+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getMaterial,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -30,10 +31,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [materialSummary, buyMethods, pickMethods] = await Promise.all([
+  const [materialSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getMaterial(client, itemId, companyId),
     getBuyMethods(client, itemId, companyId),
     getPickMethods(client, itemId, companyId),
+    getTagsList(client, companyId, "material"),
   ]);
 
   if (materialSummary.error) {
@@ -51,6 +53,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(client, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/part+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getPart,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -30,11 +31,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [partSummary, buyMethods, pickMethods] = await Promise.all([
+  const [partSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getPart(client, itemId, companyId),
-
     getBuyMethods(client, itemId, companyId),
     getPickMethods(client, itemId, companyId),
+    getTagsList(client, companyId, "part"),
   ]);
 
   if (partSummary.error) {
@@ -52,6 +53,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(client, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/tool+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getPickMethods,
   getTool,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -30,11 +31,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [toolSummary, buyMethods, pickMethods] = await Promise.all([
+  const [toolSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getTool(client, itemId, companyId),
-
     getBuyMethods(client, itemId, companyId),
     getPickMethods(client, itemId, companyId),
+    getTagsList(client, companyId, "tool"),
   ]);
 
   if (toolSummary.error) {
@@ -52,6 +53,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(client, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

Zhao39/fixfeesplatform

apps/carbon/app/routes/x+/consumable+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getItemFiles,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -32,10 +33,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [consumableSummary, buyMethods, pickMethods] = await Promise.all([
+  const [consumableSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getConsumable(serviceRole, itemId, companyId),
     getBuyMethods(serviceRole, itemId, companyId),
     getPickMethods(serviceRole, itemId, companyId),
+    getTagsList(serviceRole, companyId, "consumable"),
   ]);
 
   if (consumableSummary.error) {
@@ -53,6 +55,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(serviceRole, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/fixture+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getItemFiles,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -31,10 +32,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [fixtureSummary, buyMethods, pickMethods] = await Promise.all([
+  const [fixtureSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getFixture(serviceRole, itemId, companyId),
     getBuyMethods(serviceRole, itemId, companyId),
     getPickMethods(serviceRole, itemId, companyId),
+    getTagsList(serviceRole, companyId, "fixture"),
   ]);
 
   if (fixtureSummary.error) {
@@ -52,6 +54,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(serviceRole, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/job+/$jobId.tsx
@@ -28,6 +28,7 @@ import {
   JobHeader,
   JobProperties,
 } from "~/modules/production";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -46,8 +47,10 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   if (!jobId) throw new Error("Could not find jobId");
 
   const serviceRole = await getCarbonServiceRole();
-
-  const job = await getJob(serviceRole, jobId);
+  const [job, tags] = await Promise.all([
+    getJob(serviceRole, jobId),
+    getTagsList(serviceRole, companyId, "job"),
+  ]);
 
   if (job.error) {
     throw redirect(
@@ -58,6 +61,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
   return defer({
     job: job.data,
+    tags: tags.data ?? [],
     files: getJobDocuments(serviceRole, companyId, job.data),
     method: getJobMethodTree(serviceRole, jobId), // returns a promise
   });

apps/carbon/app/routes/x+/material+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getMaterial,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -32,10 +33,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   const { itemId } = params;
   if (!itemId) throw new Error("Could not find itemId");
 
-  const [materialSummary, buyMethods, pickMethods] = await Promise.all([
+  const [materialSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getMaterial(serviceRole, itemId, companyId),
     getBuyMethods(serviceRole, itemId, companyId),
     getPickMethods(serviceRole, itemId, companyId),
+    getTagsList(serviceRole, companyId, "material"),
   ]);
 
   if (materialSummary.error) {
@@ -53,6 +55,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(serviceRole, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/part+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getPart,
   getPickMethods,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -31,11 +32,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   if (!itemId) throw new Error("Could not find itemId");
 
   const serviceRole = await getCarbonServiceRole();
-
-  const [partSummary, buyMethods, pickMethods] = await Promise.all([
+  const [partSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getPart(serviceRole, itemId, companyId),
     getBuyMethods(serviceRole, itemId, companyId),
     getPickMethods(serviceRole, itemId, companyId),
+    getTagsList(serviceRole, companyId, "part"),
   ]);
 
   if (partSummary.error) {
@@ -53,6 +54,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(serviceRole, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 

apps/carbon/app/routes/x+/tool+/$itemId.tsx
@@ -13,6 +13,7 @@ import {
   getPickMethods,
   getTool,
 } from "~/modules/items";
+import { getTagsList } from "~/modules/shared";
 import type { Handle } from "~/utils/handle";
 import { path } from "~/utils/path";
 
@@ -31,11 +32,11 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   if (!itemId) throw new Error("Could not find itemId");
 
   const serviceRole = await getCarbonServiceRole();
-
-  const [toolSummary, buyMethods, pickMethods] = await Promise.all([
+  const [toolSummary, buyMethods, pickMethods, tags] = await Promise.all([
     getTool(serviceRole, itemId, companyId),
     getBuyMethods(serviceRole, itemId, companyId),
     getPickMethods(serviceRole, itemId, companyId),
+    getTagsList(serviceRole, companyId, "tool"),
   ]);
 
   if (toolSummary.error) {
@@ -53,6 +54,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
     files: getItemFiles(serviceRole, itemId, companyId),
     buyMethods: buyMethods.data ?? [],
     pickMethods: pickMethods.data ?? [],
+    tags: tags.data ?? [],
   });
 }
 