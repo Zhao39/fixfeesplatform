f7424f20e5189fd1789a33cc8b412a0f2312b0dd
Brad Barbin <bradbarbin@protonmail.com>
2025-12-04 07:57:37 -0500

    feat: bypass carbon users for creating new company

--- apps/erp/vite.config.ts
+++ apps/erp/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    allowedHosts: ["8d53c60dbab3.ngrok-free.app"],
+    allowedHosts: ["958d8cbb09d3.ngrok-free.app"],
   },
   plugins: [
     remix({
