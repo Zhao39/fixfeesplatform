f32130202c216f882084f99ffc9a4aa194824da7
Brad Barbin <bradbarbin@protonmail.com>
2025-12-17 15:45:01 -0500

    fix: remove posthog

--- apps/academy/app/entry.client.tsx
+++ apps/academy/app/entry.client.tsx
@@ -8,25 +8,25 @@ import { Fragment, startTransition, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 
-function PosthogInit() {
-  useEffect(() => {
-    if (VERCEL_URL && !VERCEL_URL?.includes("localhost")) {
-      posthog.init(POSTHOG_PROJECT_PUBLIC_KEY, {
-        api_host: POSTHOG_API_HOST,
-        autocapture: false,
-        capture_pageview: false
-      });
-    }
-  }, []);
-  return null;
-}
+// function PosthogInit() {
+//   useEffect(() => {
+//     if (VERCEL_URL && !VERCEL_URL?.includes("localhost")) {
+//       posthog.init(POSTHOG_PROJECT_PUBLIC_KEY, {
+//         api_host: POSTHOG_API_HOST,
+//         autocapture: false,
+//         capture_pageview: false
+//       });
+//     }
+//   }, []);
+//   return null;
+// }
 
 startTransition(() => {
   hydrateRoot(
     document,
     <Fragment>
       <HydratedRouter />
-      <PosthogInit />
+      {/* <PosthogInit /> */}
     </Fragment>
   );
 });
