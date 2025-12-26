f54441086529e3e45b7bc915209204948c83f61d
Brad Barbin <bradbarbin@protonmail.com>
2025-12-26 18:00:00 -0500

    refactor: generate types

--- packages/database/supabase/functions/lib/types.ts
+++ packages/database/supabase/functions/lib/types.ts
@@ -2257,6 +2257,8 @@ export type Database = {
           id: string
           inventoryJobCompletedNotificationGroup: string[]
           kanbanOutput: Database["public"]["Enums"]["kanbanOutput"]
+          maintenanceAdvanceDays: number
+          maintenanceGenerateInAdvance: boolean
           materialGeneratedIds: boolean
           productLabelSize: string | null
           purchasePriceUpdateTiming: Database["public"]["Enums"]["purchasePriceUpdateTiming"]
@@ -2274,6 +2276,8 @@ export type Database = {
           id: string
           inventoryJobCompletedNotificationGroup?: string[]
           kanbanOutput?: Database["public"]["Enums"]["kanbanOutput"]
+          maintenanceAdvanceDays?: number
+          maintenanceGenerateInAdvance?: boolean
           materialGeneratedIds?: boolean
           productLabelSize?: string | null
           purchasePriceUpdateTiming?: Database["public"]["Enums"]["purchasePriceUpdateTiming"]
@@ -2291,6 +2295,8 @@ export type Database = {
           id?: string
           inventoryJobCompletedNotificationGroup?: string[]
           kanbanOutput?: Database["public"]["Enums"]["kanbanOutput"]
+          maintenanceAdvanceDays?: number
+          maintenanceGenerateInAdvance?: boolean
           materialGeneratedIds?: boolean
           productLabelSize?: string | null
           purchasePriceUpdateTiming?: Database["public"]["Enums"]["purchasePriceUpdateTiming"]
@@ -46475,14 +46481,14 @@ export type Database = {
         Relationships: [
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["supplierCountryCode"]
+            columns: ["customerCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
           },
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["customerCountryCode"]
+            columns: ["supplierCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
@@ -49002,14 +49008,14 @@ export type Database = {
         Relationships: [
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["invoiceCountryCode"]
+            columns: ["customerCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
           },
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["customerCountryCode"]
+            columns: ["invoiceCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
@@ -49545,14 +49551,14 @@ export type Database = {
         Relationships: [
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["customerCountryCode"]
+            columns: ["paymentCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
           },
           {
             foreignKeyName: "address_countryCode_fkey"
-            columns: ["paymentCountryCode"]
+            columns: ["customerCountryCode"]
             isOneToOne: false
             referencedRelation: "country"
             referencedColumns: ["alpha2"]
